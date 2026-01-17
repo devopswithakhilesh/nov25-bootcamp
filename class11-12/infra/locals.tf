b851883f7b7e6ef7d3262baa8dd9d88e12ce2207
Akhilesh Mishra <165805433+devopswithakhilesh@users.noreply.github.com>
2026-01-18 01:15:26 +0530

    Add files via upload

---
+++ class11-12/infra/locals.tf
@@ -0,0 +1,19 @@
+locals {
+  apps = [
+     {
+      # what we wanted to
+        "name" : "student-portal1",
+        "image": "",
+        "port" : "",
+        "healthcheck" : ""
+    },
+    {
+      # what we build now
+        "name" : "student-portal2",
+        "image": "",
+        "port" : "",
+        "healthcheck" : ""
+    },
+
+  ]
+}
---
+++ class11-12/infra/variables.tf
@@ -0,0 +1,10 @@
+
+variable "domain_name" {
+  type = string
+  default = "akhileshmishra.tech"
+}
+
+variable "subdomain" {
+  type = string
+  default = "class11"
+}
